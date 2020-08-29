6e895768a39ac130aa6d8dd62b12a3ca2d026130
Lachlan Cresswell <lachy93@gmail.com>
2020-08-29 17:32:48 +1000

    Remove extraneous logs and format

--- bookmarklet.js
+++ bookmarklet.js
@@ -18,39 +18,34 @@ javascript: (function () {
 		for (let i = 0; i < scripts.length; i++) {
 			const s = scripts[i];
 
-			if( s.getAttribute('type') == "application/ld+json" ) {
+			if (s.getAttribute('type') == "application/ld+json") {
 				scrapedJSON = JSON.parse(s.innerText);
 
 				//allrecipes returns an array
-				if( !Array.isArray(scrapedJSON) ) { 
-					console.log('is not array');
+				if (!Array.isArray(scrapedJSON)) {
 					scrapedJSON = [scrapedJSON]; // a bit cheeky
 				}
 
-				scrapedJSON = scrapedJSON.filter( item => {
-					console.log('checking type - ' + item["@type"]);
+				scrapedJSON = scrapedJSON.filter(item => {
 					return item["@type"] == 'Recipe';
 				});
 
-				if( scrapedJSON.length ) {
+				if (scrapedJSON.length) {
 					scrapedRecipe = scrapedJSON[0];
 					break;
 				}
 			}
-			
+
 		}
 
 		// Select items from JSON
 		var recipe = {};
 		for (let i = 0; i < Object.keys(scrapedRecipe).length; i++) {
 			const key = Object.keys(scrapedRecipe)[i];
-			if ( sections.some(v => key === (v))) {
-				recipe[ key ] = scrapedRecipe[ key ];
+			if (sections.some(v => key === (v))) {
+				recipe[key] = scrapedRecipe[key];
 			}
 		}
-
-		console.clear();
-		console.log(recipe);
 
 		window.open(`http://localhost:5501/?r=${encodeURIComponent(JSON.stringify(recipe))}`);
 	})();
